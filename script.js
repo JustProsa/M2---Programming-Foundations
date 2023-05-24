cae95e88c7057de99a2a4da01fab9960eae203bf
JustProsa <90556911+JustProsa@users.noreply.github.com>
2023-05-24 19:13:46 +0200

    M2-D1

---
+++ script.js
@@ -0,0 +1,3 @@
+// 1. Che cos'è un algoritmo? Prova a spiegarlo a parole tue, come se lo dovessi spiegare a qualcuno che non capisce nulla di informatica.
+// 2. Che cos'è una variabile?
+// 3. undefined e null sono la stessa cosa?
