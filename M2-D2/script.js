df964c7648b1065508de9b7915d40789a52a16c3
JustProsa <90556911+JustProsa@users.noreply.github.com>
2023-05-25 10:09:42 +0200

    M2-D2

---
+++ M2-D2/script.js
@@ -0,0 +1,31 @@
+//Esercizio 1
+
+//Esercizio 2
+let num1 = 12;
+let num2 = 20;
+let num3 = num1 + num2;
+console.log(num3);
+
+//Esercizio 3
+let x = 12;
+
+//Esercizio 4
+let name = "Pietro";
+
+//Esercizio 5
+let subtraction = 4 - x;
+console.log(subtraction);
+
+//Esercizio 6
+const person = {
+  name: "Pietro",
+  surname: "Rosano",
+  hobby: "Scacchi",
+};
+console.log(person);
+
+//Esercizio 7
+let name1 = "john";
+let name2 = "John";
+console.log(name1 == name2);
+console.log(name1 == name2.toLowerCase());
