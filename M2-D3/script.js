b4846d40cd0817a0835409008b12649c2f39bae0
JustProsa <90556911+JustProsa@users.noreply.github.com>
2023-05-30 20:47:41 +0200

    M2-D3 complete

--- M2-D3/script.js
+++ M2-D3/script.js
@@ -32,29 +32,43 @@ if (number < 5) {
 
 //Esercizo 3
 
-let n = 0;
-console.log(n);
+// let n = 0;
+// console.log(n);
 
-while (n <= 9) {
-  n++;
+// while (n <= 9) {
+//   n++;
+//   if (n === 3 || n === 8) {
+//     continue;
+//   }
+//   console.log(n);
+// }
+
+for (let n = 0; n <= 10; n++) {
   if (n === 3 || n === 8) {
     continue;
   }
-  console.log(n);
+  console.log("Iterazione numero: " + n);
 }
 
 //Esercizio 4
 
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-for (n in numbers) {
-  if (n % 2 == 0) {
-    console.log(n + " is even");
+// for (n in numbers) {
+//   if (n % 2 == 0) {
+//     console.log(n + " is even");
+//   } else {
+//     console.log(n + " is odd");
+//   }
+// }
+
+for (let a = 0; a <= 15; a++) {
+  if (a % 2 == 0) {
+    console.log(a + " is even");
   } else {
-    console.log(n + " is odd");
+    console.log(a + " is odd");
   }
 }
-
 //Esercizi facoltativi
 //Esercizio 1
 
@@ -65,8 +79,58 @@ console.log(firstNum + " and " + secondNum);
 if (firstNum === 8 || secondNum === 8) {
   console.log("One of them is 8");
 }
-if (firstNum + secondNum == 8) {
+if (firstNum + secondNum === 8) {
   console.log("Their addiction is equal to 8");
-} else if (firstNum - secondNum == 8 || secondNum - firstNum == 8) {
+} else if (firstNum - secondNum === 8 || secondNum - firstNum === 8) {
   console.log("Their subtraction is equal to 8");
+}
+
+// Esercizio 2
+
+let totalShoppingCart = 20;
+
+if (totalShoppingCart > 50) {
+  console.log(
+    "Your cart is $" + totalShoppingCart + "! Your shipping is FREE!"
+  );
+} else {
+  console.log("The shipping is $10, the total is $" + (totalShoppingCart + 10));
+}
+
+// Esercizio 3
+
+let totalShoppingCart2 = 80;
+let totalShoppingCartBlack =
+  totalShoppingCart2 - (totalShoppingCart2 * 20) / 100;
+
+console.log(totalShoppingCartBlack);
+
+if (totalShoppingCartBlack > 50) {
+  console.log(
+    "Your cart is $" + totalShoppingCartBlack + "! Your shipping is FREE!"
+  );
+} else {
+  console.log(
+    "The shipping is $10, the total is $" + (totalShoppingCartBlack + 10)
+  );
+}
+
+// Esercizio 4
+
+let isMale = true;
+let gender = isMale ? "Male" : "Female";
+console.log(gender);
+
+// Esercizio 5
+
+for (buzzy = 1; buzzy <= 100; buzzy++) {
+  if (buzzy % 3 === 0 && buzzy % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (buzzy % 3 === 0) {
+    console.log("Fizz");
+  } else if (buzzy % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(buzzy);
+  }
 }
